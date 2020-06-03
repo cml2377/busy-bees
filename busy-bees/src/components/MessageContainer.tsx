d5f74bb422f8155c6c6659e7008594c653b6f372
Crystal Ly <crystal.m.ly@gmail.com>
2020-06-03 14:34:04 -0500

    added static demo for messageContainer and Todos

--- busy-bees/src/components/MessageContainer.tsx
+++ busy-bees/src/components/MessageContainer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './MessageContainer.css';
+import { IonList, IonItem, IonLabel, IonContent, IonAvatar } from '@ionic/react';
 
 interface ContainerProps {
   name: string;
@@ -7,18 +8,92 @@ interface ContainerProps {
 
 const MessageContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      <br></br>
-      <strong>Theme Colors:</strong>
-      <ul>
-        <li style={{ listStyle: "none", }}>Yellow: #ffc066</li>
-        <li style={{ listStyle: "none", }}>Dark blue-black: #263740</li>
-        <li style={{ listStyle: "none", }}>Light gray-blue: #C2D5D8</li>
-      </ul>
-    </div >
+    <IonContent>
+      {/*-- List of Text Items --*/}
+      <IonList className="messageContainer">
+        <IonItem>
+          <IonAvatar slot="start"><svg width="40" height="40" data-jdenticon-value="user127"></svg></IonAvatar>
+          <IonLabel>
+            <h2>Crystal</h2>
+            <h3>Creator, Lo-Mein Coder</h3>
+            <p>Sure! That sounds like a great idea...</p>
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonAvatar slot="start"><svg width="40" height="40" data-jdenticon-value="user553"></svg></IonAvatar>
+          <IonLabel>
+            <h2>Brianna</h2>
+            <h3>Fuckin Awesome</h3>
+            <p>Yeah, so just make kissy sounds at cats to...</p>
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonAvatar slot="start"><svg width="40" height="40" data-jdenticon-value="user355"></svg></IonAvatar>
+          <IonLabel>
+            <h2>Anthony</h2>
+            <h3>High Standards in Code and Life</h3>
+            <p>I don't think that would work...</p>
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonAvatar slot="start"><svg width="40" height="40" data-jdenticon-value="Z"></svg></IonAvatar>
+          <IonLabel>
+            <h2>Z</h2>
+            <h3>AKA Zaddy</h3>
+            <p>Raven has been doing so well in training...</p>
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonAvatar slot="start"><svg width="40" height="40" data-jdenticon-value="user126"></svg></IonAvatar>
+          <IonLabel>
+            <h2>Josh</h2>
+            <h3>Pastor Josh</h3>
+            <p>Where in the settings can I change...</p>
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonAvatar slot="start"><svg width="40" height="40" data-jdenticon-value="Israel"></svg></IonAvatar>
+          <IonLabel>
+            <h2>Israel</h2>
+            <h3>HBIC</h3>
+            <p>LOL</p>
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonAvatar slot="start"><svg width="40" height="40" data-jdenticon-value="Mags"></svg></IonAvatar>
+          <IonLabel>
+            <h2>Mags</h2>
+            <h3>Deal With It</h3>
+            <p>We're picking up the puppers on...</p>
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonAvatar slot="start"><svg width="40" height="40" data-jdenticon-value="Dan"></svg></IonAvatar>
+          <IonLabel>
+            <h2>Dan</h2>
+            <h3>The Only Sensible One Here</h3>
+            <p>I think I'll be able to make it!</p>
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonAvatar slot="start"><svg width="40" height="40" data-jdenticon-value="Justin"></svg></IonAvatar>
+          <IonLabel>
+            <h2>Justin</h2>
+            <h3>Not Here to be Average</h3>
+            <p>Sounds good, I'll let you know...</p>
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonAvatar slot="start"><svg width="40" height="40" data-jdenticon-value="Justin"></svg></IonAvatar>
+          <IonLabel>
+            <h2>Kimi</h2>
+            <h3>Working Hard</h3>
+            <p>We might be able to make it!!!</p>
+          </IonLabel>
+        </IonItem>
+      </IonList>
+    </IonContent>
   );
-};
+}
 
 export default MessageContainer;
--- busy-bees/src/components/ToDoContainer.tsx
+++ busy-bees/src/components/ToDoContainer.tsx
@@ -1,23 +1,57 @@
 import React from 'react';
+import { useState } from 'react';
 import './ToDoContainer.css';
-
+import { IonList, IonItem, IonLabel, IonCheckbox, IonContent, IonPage, IonHeader, IonToolbar, IonItemDivider, IonTitle } from '@ionic/react';
 interface ContainerProps {
     name: string;
 }
 
+const checkboxList = [
+    { val: 'Do Laundry', isChecked: true },
+    { val: 'Finish Busy Bees', isChecked: false },
+    { val: 'Work with Anthony to figure out Drive Thru App', isChecked: false }
+];
+
 const ToDoContainer: React.FC<ContainerProps> = ({ name }) => {
+    const [checked, setChecked] = useState(false);
+
     return (
-        <div className="container">
-            <strong>{name}</strong>
-            <p>To Do Container</p>
-            <br></br>
-            <strong>Theme Colors:</strong>
-            <ul>
-                <li style={{ listStyle: "none", }}>Yellow: #ffc066</li>
-                <li style={{ listStyle: "none", }}>Dark blue-black: #263740</li>
-                <li style={{ listStyle: "none", }}>Light gray-blue: #C2D5D8</li>
-            </ul>
-        </div >
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>CheckboxExamples</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonList>
+                    <IonItemDivider>Default Checkbox</IonItemDivider>
+                    <IonItem>
+                        <IonLabel>Checked: {JSON.stringify(checked)}</IonLabel>
+                        <IonCheckbox checked={checked} onIonChange={e => setChecked(e.detail.checked)} />
+                    </IonItem>
+
+                    <IonItemDivider>Disabled Checkbox</IonItemDivider>
+                    <IonItem><IonCheckbox slot="end" disabled={true} /></IonItem>
+
+                    <IonItemDivider>Checkbox Colors</IonItemDivider>
+                    <IonItem>
+                        <IonCheckbox slot="end" color="primary" />
+                        <IonCheckbox slot="end" color="secondary" />
+                        <IonCheckbox slot="end" color="danger" />
+                        <IonCheckbox slot="end" color="light" />
+                        <IonCheckbox slot="end" color="dark" />
+                    </IonItem>
+                    <IonItemDivider>Checkboxes in a List</IonItemDivider>
+
+                    {checkboxList.map(({ val, isChecked }, i) => (
+                        <IonItem key={i}>
+                            <IonLabel>{val}</IonLabel>
+                            <IonCheckbox slot="end" value={val} checked={isChecked} />
+                        </IonItem>
+                    ))}
+                </IonList>
+            </IonContent>
+        </IonPage>
     );
 };
 
